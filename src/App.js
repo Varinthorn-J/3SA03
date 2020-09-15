7c608e6f4325e08db4012e3d2666a084288bd703
warinthon30 <jaitrong3016@gmail.com>
2020-09-15 17:25:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+
+function App() {
+  return (
+  <div>
+  <CharacterCard value="h"/>
+  <CharacterCard value="i"/>
+  </div>
+  );
+ }
+   
+
+export default App;
